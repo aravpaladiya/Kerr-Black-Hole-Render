1131e954d63898abecc68098c6eb90035337860d
aravp <arav4pals@gmail.com>
2024-04-30 22:17:17 -0400

    test

--- GraphicsEngine/engine.cpp
+++ GraphicsEngine/engine.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "shader_h.h"
 #include <windows.h>
-#include <C:\\Users\\aravp\\source\\repos\\GraphicsEngine\\GraphicsEngine\\stb_image.h>
+#include "stb_image.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
